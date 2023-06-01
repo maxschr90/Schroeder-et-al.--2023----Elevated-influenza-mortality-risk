c896ba45b66691a1941517741dd12969f234ce0a
rebeccamancy <rebecca.mancy@gmail.com>
2023-06-01 15:46:53 +0100

    Finalised Fig A-2

--- Code/Fig_Scatter.R
+++ Code/Fig_Scatter.R
@@ -1,47 +1,43 @@
-# Script to make scatter plots of epidemic years
+# Script to make scatter plots of epidemic years, Figure A-2
 
 # Set working directory
-setwd("/Users/rebeccamancy/Developer/Schroeder-et-al.-2021--How-long-do-pandemics-last-/")
-getwd()
+setwd("~/Developer/Schroeder-et-al.--2023----Elevated-influenza-mortality-risk/Code")
 rm(list = ls())
 
 library(tidyr) # gather
 library(dplyr) # joins
 library(readxl) # For reading in data from Excel
-#library(writexl)
 library(ggplot2) 
-library('ggthemes')
-#library(cowplot)
+library(lemon)
 
-figure.path <- "Figures/"
+figure.path <- "../Figures"
 
-# ---- Select Load up dataset ----
-influenza <- read_excel(path = "Data/Relative Mortality.xlsx", sheet = "Influenza")
-names(influenza)[1] <- "Year"
-respiratory <- read_excel(path = "Data/Relative Mortality.xlsx", sheet = "Respiratory")
-names(respiratory)[1] <- "Year"
+# ---- Load up dataset ----
+influenza <- read_excel(path = "SI_Figure_A2_Data.xlsx", sheet = "Influenza")
+respiratory <- read_excel(path = "SI_Figure_A2_Data.xlsx", sheet = "Respiratory")
 
+# Push into long format for ggplot
 flu <- gather(data = influenza, "City", "Influenza", -Year)
-#flu$variable <- "Influenza"
-
 resp <- gather(data = respiratory, "City", "Respiratory", -Year)
-#resp$variable <- "Respiratory"
 
+# Join flu and respiratory data together into a single dataframe for plotting
 dat <- left_join(x = flu, y = resp)
 head(dat)
   
-library(lemon)
+# Make figure
 p <- ggplot(dat) + 
-  geom_point(aes(x=Influenza, y=Respiratory, colour = City), 
-            size = 1, shape = 1) +
+  geom_point(aes(x=Influenza, y=Respiratory, shape = City), 
+            size = 1) +
   scale_x_continuous("Relative mortality (influenza)", breaks = seq(0,8,by=2), labels = seq(0,8,by=2), limits = c(0,8.5)) +
   scale_y_continuous("Relative mortality (respiratory tract diseases)", breaks = seq(0,2.5, by=0.5), labels = seq(0,2.5, by=0.5)) +
+  scale_shape_manual(values = c(3,22,11,0,5,8,6,2)) + 
   theme_classic() + 
   theme(strip.background = element_blank(), 
         strip.text.x = element_text(size = 12, face = "bold"),
         panel.spacing = unit(1, "lines"))
 p <- p + facet_rep_wrap(~Year) #, repeat.tick.labels = 'left') 
+p
 
-ggsave(plot = p, filename = "Fig_Excess_Mortality.pdf", path = figure.path, 
+ggsave(plot = p, filename = "SI_Figure_A-2.pdf", path = figure.path, 
        width = 17, height = 10, units = "cm")
 
